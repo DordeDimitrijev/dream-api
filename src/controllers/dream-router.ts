cd5e97a8edbe54e276b183329b81db680684f25f
djoled97 <46656919+djoled97@users.noreply.github.com>
2021-03-13 15:06:23 +0100

    Add Delete update and getbyID for dream

--- src/controllers/dream-router.ts
+++ src/controllers/dream-router.ts
@@ -1,26 +1,70 @@
-import {Request, Response, Router} from "express";
+import { Request, Response, Router } from "express";
 import { IDream, Dream } from "../schemas/Dream";
 
-const router:Router = Router();
+const router: Router = Router();
 
-router.post('/', async (req:Request,res:Response)=>{
-    try{
-        const newDream:IDream = await  Dream.create(req.body);
+router.post('/', async (req: Request, res: Response) => {
+    try {
+        const newDream: IDream = await Dream.create(req.body);
         return res.status(200).send(newDream);
     }
-   catch (err){
-       console.log(err);
-       return res.status(400).send();
-   }
+    catch (err) {
+        console.log(err);
+        return res.status(400).send({ message: err._message });
+    }
 
 
 })
 
-router.get('/', async (req:Request,res:Response)=>{
+router.get('/', async (req: Request, res: Response) => {
 
-const getAllDreams:IDream[] = await Dream.find();
+    const getAllDreams: IDream[] = await Dream.find().lean();
 
-return res.status(200).send(getAllDreams);
+    return res.status(200).send(getAllDreams);
+
+
+})
+
+router.get('/:id', async (req: Request, res: Response) => {
+    const id: string = req.params.id;
+   
+   try{ const dream: IDream = await Dream.findById(id).lean();
+
+    return res.status(200).send(dream);
+   }
+   catch(err){
+       return res.status(400).send({message: "Dream with the ID not found"});
+   }
+
+})
+
+router.delete('/:id', async (req: Request, res: Response) => {
+    const id: string = req.params.id;
+    try {
+        const deleteDream: IDream = await Dream.findByIdAndDelete(id).lean();
+
+        return res.status(200).send(deleteDream);
+    }
+    catch (err) {
+        return res.status(400).send({ message:  "Dream with the ID not found" });
+    }
+
+
+
+})
+
+router.put('/:id', async (req: Request, res: Response) => {
+    const id: string = req.params.id;
+
+    try {
+        const updatedDream: IDream = await Dream.findByIdAndUpdate(id, req.body, { new: true }).lean();
+
+        return res.status(200).send(updatedDream);
+    }
+    catch (err) {
+        return res.status(400).send({ message: err._message });
+    }
+
 
 
 })
